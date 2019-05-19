a82eb6e02979eafc2984be374c8b7e992aa0107a
Varun Sharma <varunsharma38@live.com>
2019-05-19 20:09:14 +0530

    add options skeleton on UI

--- src/App.js
+++ src/App.js
@@ -2,10 +2,29 @@ import React from 'react';
 
 export default class App extends React.Component {
 
-    render() {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showOptions: true
+        }
+        this._renderOptions = this._renderOptions.bind(this);
+    }
+
+    _renderOptions = () => {
         return (
             <div>
-                <h3>React Webpack Test App</h3>
+                <button className='mt-2 d-block'>Create New Transaction</button>
+                <button className='mt-2 d-block'>Mine Pending Transactions</button>
+                <button className='mt-2 d-block'>View The Chain</button>
+                <button className='mt-2 d-block'>View Node Balance</button>
+            </div>
+        )
+    }
+
+    render() {
+        return (
+            <div className='mt-4'>
+                {this.state.showOptions ? this._renderOptions() : null}
             </div>
         );
     }
